b7f9ddb7973151406c164130bf7764509153afde
abdullohumar <iniautok@gmail.com>
2026-04-24 15:31:39 +0700

    feat: persona AI (system instruction) update: change AI model ( model before doesn't exists anymore)

--- app/Services/GeminiService.php
+++ app/Services/GeminiService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Exception;
+use Gemini\Data\Content;
 use Gemini\Laravel\Facades\Gemini;
 
 class GeminiService
@@ -14,7 +15,17 @@ class GeminiService
     public function generateText(string $prompt): string
     {
         try {
-            $result = Gemini::geminiPro()->generateContent($prompt);
+            // Kita definisikan instruksi sistem (Persona AI)
+            $systemText = "Anda adalah Senior Backend Developer yang ahli dalam Laravel.
+                                  Jawablah pertanyaan dengan teknis, singkat, dan gunakan analogi kopi.";
+
+            /**
+             * Recursive Learning: 
+             * Kita tetap menggunakan model 'gemini-2.5-flash' yang tadi sudah terbukti jalan.
+             * Kita tambahkan method withSystemInstruction.
+             */
+            $systemInstruction = Content::parse($systemText);
+            $result = Gemini::generativeModel('gemini-2.5-flash')->withSystemInstruction($systemInstruction)->generateContent($prompt);
 
             return $result->text();
         } catch (Exception $e) {
